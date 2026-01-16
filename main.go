542b3032cdf328e478ea9649b93562cdded12a74
shalshcode08 <somushrestha448@gmail.com>
2026-01-16 15:50:41 +0530

    update: initial commit for setting up model,view,update(MVU) arch

--- main.go
+++ main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+type model struct {
+	msg string
+}
+
+func initializeModel() model {
+	return model{
+		msg: "hello",
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	case tea.KeyMsg:
+		switch msg.String() {
+
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	var style = lipgloss.NewStyle().
+		Bold(true).
+		Foreground(lipgloss.Color("16")).
+		Background(lipgloss.Color("205")).
+		PaddingLeft(2).
+		PaddingRight(2)
+
+	welcome := style.Render("Welcome to TERMNOTE 🗒️")
+
+	helpKeys := "Ctrl+N : new file . Ctrl+L: list . Esc: back/save . Ctrl+S: save . Ctrl+Q: quit"
+
+	view := ""
+
+	return fmt.Sprintf("\n%s\n\n%s\n\n%s", welcome, view, helpKeys)
+
+}
 
 func main() {
-	fmt.Println("welcome to go")
+	p := tea.NewProgram(initializeModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
 }
